a455296e096febe4c66b4be27f03b706685683d9
RicardoTIC <ricardo.tic@outlook.es>
2020-07-16 01:44:23 -0600

    Commit Inicial

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+
+$arregloDatos =  array("Nombre"=>"Ricardo","Apellidos"=>"Herrera","Telefono"=>"(656)358-8226");
+
+for($arregloDatos as $rows) {
+	
+	echo "Estos son los datos de mi arreglo " . $rows ." <br> ";
+
+}
+
+
+?>
