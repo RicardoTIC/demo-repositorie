f646c39cb03405c8b0b54cf881c7e3c2f5500298
RicardoTIC <ricardo.tic@outlook.es>
2020-07-16 01:57:20 -0600

    Se realizo la siguiente modificacion

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
 <?php 
 
+$nombre = "Ricardo";
+
 $arregloDatos =  array("Nombre"=>"Ricardo","Apellidos"=>"Herrera","Telefono"=>"(656)358-8226");
 
-for($arregloDatos as $rows) {
+foreach($arregloDatos as $rows) {
 	
 	echo "Estos son los datos de mi arreglo " . $rows ." <br> ";
 
 }
+
+
+echo "<p style='color:red;'>El siguiente parentisis es : </p>" . $nombre;
+echo "<p style='color:red;'>El siguiente parentisis es : </p>" . $nombre;
 
 
 ?>
